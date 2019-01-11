ad5e7dbb1be4fa078df8cc0c1086084e036b248d
Kyle Sekellick <sekellickkyle@yahoo.com>
2019-01-10 23:04:01 -0500

    adds correct text for testing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export default class Hello360 extends React.Component {
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
           <Text style={styles.greeting}>
-            Welcome to React 360
+            360 Test
           </Text>
         </View>
       </View>
