f0fe8a174488521a3a2ac1895fa0d689acaeda18
Kyle Sekellick <sekellickkyle@yahoo.com>
2019-01-11 23:06:22 -0500

    adds second example test for react360

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-import {ReactInstance} from 'react-360-web';
+import {ReactInstance, Location, Surface} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -6,16 +6,26 @@ function init(bundle, parent, options = {}) {
     ...options,
   });
 
+  // Create three roots: two flat panels on the left and the right, and a Location
+  // to mount rendered models in 3D space
+  const leftPanel = new Surface(300, 600, Surface.SurfaceShape.Flat);
+  leftPanel.setAngle(-0.6, 0);
+  const rightPanel = new Surface(300, 600, Surface.SurfaceShape.Flat);
+  rightPanel.setAngle(0.6, 0);
   r360.renderToSurface(
-    r360.createRoot('SlideshowSample', {
-      photos: [
-        {uri: './static_assets/360_world.jpg', title: '360 World', format: '2D'},
-        // Add your own 180 / 360 photos to this array,
-        // with an associated title and format
-      ],
-    }),
-    r360.getDefaultSurface(),
+    r360.createRoot('TopPosts'),
+    leftPanel,
   );
+  r360.renderToSurface(
+    r360.createRoot('CurrentPost'),
+    rightPanel,
+  );
+  r360.renderToLocation(
+    r360.createRoot('ModelView'),
+    new Location([0, -2, -10]),
+  );
+
+  r360.compositor.setBackground('./static_assets/360_world.jpg');
 }
 
 window.React360 = {init};
